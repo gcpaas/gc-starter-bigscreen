852057de02bc7ffafc01cdad86294e45f65b24d5
hong.yang <hong.yang@ustcinfo.com>
2023-05-16 09:32:04 +0800

    fix: 数据集分类树添加权限校验注解
    数据集分类树添加权限校验注解

--- gc-starter-bigscreen-core/src/main/java/com/gccloud/plugin/ds/controller/CategoryTreeController.java
+++ gc-starter-bigscreen-core/src/main/java/com/gccloud/plugin/ds/controller/CategoryTreeController.java
@@ -18,6 +18,7 @@ import java.util.List;
  * @author pan.shun
  * @since 2021/9/7 11:05
  */
+@ScreenPermission(permissions = {Permission.DataSet.CATEGORY})
 @Api(tags = "种类树")
 @RestController
 @RequestMapping("/category")
@@ -26,21 +27,18 @@ public class CategoryTreeController extends SuperController {
     @Resource
     private CategoryTreeService categoryTreeService;
 
-    @ScreenPermission(permissions = {Permission.DataSet.CATEGORY})
     @ApiOperation("依据类型查询对应的种类树")
     @GetMapping("/queryTreeList")
     public R<List<CategoryTree>> queryTreeList(String tableName, String moduleCode, Integer ifFilter) {
         return R.success(categoryTreeService.queryCategoryTree(tableName, moduleCode, ifFilter));
     }
 
-    @ScreenPermission(permissions = {Permission.DataSet.CATEGORY})
     @ApiOperation("种类树新增或修改")
     @PostMapping("/addOrUpdateTree")
     public void addOrUpdateTree(@RequestBody CategoryTree categoryTree) {
         categoryTreeService.addOrUpdateTree(categoryTree);
     }
 
-    @ScreenPermission(permissions = {Permission.DataSet.CATEGORY})
     @ApiOperation("根据ID删除种类树")
     @GetMapping("/remove/{id}")
     public void removeNode(@PathVariable String id) {
